2f264b8955a68b8d9f170106a1a593ebb36b9794
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-10 19:11:11 +0530

    EdgeLedger Website

--- html-css-course/edgeledger-website/js/main.js
+++ html-css-course/edgeledger-website/js/main.js
@@ -13,6 +13,14 @@ function initMap() {
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
 $("#navbar a, .btn").on("click", function(event) {
   if (this.hash !== "") {
     event.preventDefault();
